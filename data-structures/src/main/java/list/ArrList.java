3973d1a7ebffb8371903afc50a9aa4913726e983
HyeonJung Lee <hj.lee@wafour.com>
2022-03-15 17:41:00 +0900

    3/15 study ArrayList implement

--- data-structures/src/main/java/list/ArrList.java
+++ data-structures/src/main/java/list/ArrList.java
@@ -2,7 +2,8 @@ package list;
 
 import java.util.NoSuchElementException;
 
-public class ArrList <E> {
+public class ArrList<E> {
+
     private E a[];      // 리스트의 항목들을 저장할 배열
     private int size;   // 리스트의 항목 수
 
@@ -12,8 +13,35 @@ public class ArrList <E> {
     }
 
     public E peek(int k) {  // k번째 항목을 리턴, 단순히 읽기만 한다.
-        if ( size == 0 )
+        if (size == 0) {
             throw new NoSuchElementException(); // underflow 경우에 프로그램 정지
+        }
         return a[k];
+    }
+
+    public void insertLast(E newItem) {
+        if (size == a.length) {     // 배열에 빈 공간이 없으면 배열 크기 확장
+            resize(2 * a.length);
+        }
+        a[size++] = newItem;        // 새 항목 삽입
+    }
+
+    public void insert(E newItem, int k) {  // 새 항목을 k-1번째 항목 다음에 삽입
+        if (size == a.length) {
+            resize(2 * a.length);
+        }
+        for (int i = size - 1; i >= k; i--) {   // 한칸씩 뒤로 이동
+            a[i + 1] = a[i];
+        }
+        a[k] = newItem;
+        size++;
+    }
+
+    private void resize(int newSize) {      // 배열 크기 조절
+        Object[] t = new Object[newSize];   // newSize 크기의 새로운 배열 t 생성
+        for (int i = 0; i < size; i++) {
+            t[i] = a[i];                    // 배열 s를 배열 t로 복사
+        }
+        a = (E[]) t;                        // 배열 t를 배열 s로
     }
 }
