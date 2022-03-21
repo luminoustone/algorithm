cba6b3524afad779baf1588560ac7a177a1c77b1
mlicp1994 <mlicp1994@gmail.com>
2022-03-21 23:03:52 +0900

    3/21 study DoublyLinkedList implement

---
+++ data-structures/src/main/java/list/DoublyLinkedList.java
@@ -0,0 +1,42 @@
+package list;
+
+import java.util.NoSuchElementException;
+
+public class DoublyLinkedList <E>{
+    protected DNode head, tail;
+    protected int size;
+
+    public DoublyLinkedList() {
+        head = new DNode(null, null, null);
+        tail = new DNode(null, head, null);
+        head.setNext(tail);
+        size = 0;
+    }
+
+    public void insertBefore(DNode p, E newItem) {
+        DNode t = p.getPrevious();
+        DNode newNode = new DNode(newItem, t, p);
+        p.setPrevious(newNode);
+        t.setNext(newNode);
+        size++;
+    }
+
+    public void insertAfter(DNode p, E newItem){
+        DNode t= p.getNext();
+        DNode newNode = new DNode(newItem, p, t);
+        t.setPrevious(newNode);
+        p.setNext(newNode);
+        size++;
+    }
+
+    public void delete(DNode x) {
+        if (x ==) {
+            throw new NoSuchElementException();
+        }
+        DNode f = x.getPrevious();
+        DNode r = x.getNext();
+        f.setNext(r);
+        r.setPrevious(f);
+        size--;
+    }
+}
